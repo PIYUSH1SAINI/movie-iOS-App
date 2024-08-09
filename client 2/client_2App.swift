2309cbcf069ad0b65bc3c8ceb38fbe2aa2d137f3
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-08-09 10:23:55 +0530

    Initial Commit

---
+++ client 2/client_2App.swift
@@ -0,0 +1,17 @@
+//
+//  client_2App.swift
+//  client 2
+//
+//  Created by Piyush saini on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct client_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
